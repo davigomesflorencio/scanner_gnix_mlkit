61ec54f6af313a311d4a457c3439277183a30b0d
davigomesflorencio <davigomesflorencio@gmail.com>
2026-01-27 16:16:40 -0300

    build - Update version Code 1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.davi.dev.scannermlkit"
         minSdk = 31
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
