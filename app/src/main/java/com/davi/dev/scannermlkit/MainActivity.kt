9a1d55bdc77e49a129e45ffb90eff6808e8d304e
davigomesflorencio <davigomesflorencio@alu.ufc.br>
2026-02-26 21:36:20 -0300

    feat: Implement core document and QR code scanning with PDF viewing and merging capabilities.

--- app/src/main/java/com/davi/dev/scannermlkit/MainActivity.kt
+++ app/src/main/java/com/davi/dev/scannermlkit/MainActivity.kt
@@ -4,14 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.davi.dev.scannermlkit.presentation.navigation.AppNavHost
+import com.davi.dev.scannermlkit.presentation.screens.viewModel.MergeDocumentViewModel
+import com.davi.dev.scannermlkit.presentation.screens.viewModel.ScannerDocumentViewModel
+import com.davi.dev.scannermlkit.presentation.screens.viewModel.ScannerQrCodeViewModel
 import com.davi.dev.scannermlkit.presentation.theme.ScannermlkitTheme
 
 class MainActivity : ComponentActivity() {
+
+    val scannerQrCodeViewModel: ScannerQrCodeViewModel by viewModels()
+    val scannerDocumentViewModel: ScannerDocumentViewModel by viewModels()
+    val mergeDocumentViewModel: MergeDocumentViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,7 +31,11 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.surface
                 ) {
-                    AppNavHost()
+                    AppNavHost(
+                        scannerQrCodeViewModel = scannerQrCodeViewModel,
+                        scannerDocumentViewModel = scannerDocumentViewModel,
+                        mergeDocumentViewModel = mergeDocumentViewModel
+                    )
                 }
             }
         }
