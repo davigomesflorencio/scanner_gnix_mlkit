ce81edc3a0ff655a0f21ee9769a4f9def1a323d8
davigomesflorencio <davigomesflorencio@gmail.com>
2026-03-01 15:50:00 -0300

    feat: Implement a splash screen, introduce a custom circular progress component, and apply minor UI refinements to PDF viewers and watermark opacity.

---
+++ app/src/main/java/com/davi/dev/scannermlkit/presentation/components/CustomCircularProgress.kt
@@ -0,0 +1,36 @@
+package com.davi.dev.scannermlkit.presentation.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.tooling.preview.Preview
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
+import com.davi.dev.scannermlkit.presentation.screens.splash.SplashScreen
+import com.davi.dev.scannermlkit.presentation.theme.ScannermlkitTheme
+
+@Composable
+fun CustomCircularProgress() {
+    val composition by rememberLottieComposition(
+        LottieCompositionSpec.Asset("loading.json") )
+
+    val progress by animateLottieCompositionAsState(
+        composition = composition,
+        iterations = LottieConstants.IterateForever
+    )
+
+    LottieAnimation(
+        composition = composition,
+        progress = { progress }
+    )
+}
+
+@Preview
+@Composable
+fun previewCustomCircularProgress() {
+    ScannermlkitTheme {
+        CustomCircularProgress()
+    }
+}
